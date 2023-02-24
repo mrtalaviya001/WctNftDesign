385861f97ddfc9ee2a10e8b8108fdcaba579283c
mrtalaviya001 <talaviyajaydeep001@gmail.com>
2023-02-24 13:50:07 +0530

    change

---
+++ components/Header.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import React from "react";
+import NFT from "../assets/Header/NFT.png";
+import Round from "../assets/Header/Round.png";
+import Rectangle from "../assets/Header/Rectangle.png";
+import Trade from "../assets/Header/Trade.png";
+import Sales from "../assets/Header/Sales.png";
+
+const Header = () => {
+  return (
+    <header className="mt-4">
+      <div>
+        <div className="grid grid-cols-5 pl-2  gap-4 ">
+          <div className="bg-[#c0eec0] flex justify-center items-center h-[151px] rounded-md ">
+            <Image src={NFT} alt={""} className="cursor-pointer" />
+          </div>
+
+          <div className="bg-[#fdf7d5] flex justify-center items-center h-[151px] rounded-md ">
+            <Image src={Round} alt={""} className="cursor-pointer" />
+          </div>
+          <div className="bg-[#d6f3f8] flex justify-center items-center h-[151px] rounded-md ">
+            <Image src={Rectangle} alt={""} className="cursor-pointer" />
+          </div>
+          <div className="bg-[#eddbf4] flex justify-center items-center h-[151px] rounded-md ">
+            <Image src={Trade} alt={""} className="cursor-pointer" />
+          </div>
+          <div className="bg-[#fcdcef] flex justify-center items-center h-[151px] rounded-md ">
+            <Image src={Sales} alt={""} className="cursor-pointer" />
+          </div>
+        </div>
+        <div className="grid grid-cols-5 mt-1  gap-4 ">
+          <div className=" flex font-bold justify-center items-start h-[151px] rounded-md ">
+            NFT Management
+          </div>
+
+          <div className=" flex font-bold justify-center items-start">
+            Find Holders
+          </div>
+          <div className=" flex font-bold justify-center items-start">
+            Airdrop
+          </div>
+          <div className=" flex font-bold justify-center items-start">
+            Trade-in
+          </div>
+          <div className=" flex font-bold justify-center items-start">
+            Sales
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,32 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Navbar from "../components/Navbar";
-import Homepage from "../assets/Logo/CD Home.png"
+import Homepage from "../assets/Logo/CD Home.png";
+import Header from "../components/Header";
 
 const Home: NextPage = () => {
   return (
     <>
-      <div className="mx-4">
-        <Navbar />
+      <div>
+        <div className="mx-4">
+          <Navbar />
+          <Header />
+        </div>
+        <div className="bg-[#F5F5F5]">
+          <div className="mx-4">
+            <div className="grid grid-cols-4 gap-4">
+              <div className="col-span-3">
+                <p className="HeadingTag">Events</p>
+              </div>
+              <div className="...">
+                <p className="HeadingTag">Overview</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-      <div >
+
+      <div>
         <Image src={Homepage} alt="Logo" className="w-full" />
       </div>
     </>
